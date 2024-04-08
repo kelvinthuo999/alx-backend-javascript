1a53ec5a2f3a7f158f365f5f49df3fc1e9435aa2
kelvinthuo999 <thuokelvinngigi@gmail.com>
2024-04-08 12:57:31 -0700

    create: function to return a previously created object

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,8 +1,14 @@
 export default function createReportObject(employeesList) {
+  const allEmployees = {};
+
+  for (const [departmentName, employees] of Object.entries(employeesList)) {
+    allEmployees[departmentName] = [...employees];
+  }
+
   return {
-    allEmployees: employeesList,
+    allEmployees,
     getNumberOfDepartments() {
       return Object.keys(employeesList).length;
-    }
+    },
   };
 }
