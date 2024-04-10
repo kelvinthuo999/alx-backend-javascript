4ef3a1f12582741ca609081a48110a79b7d94262
kelvinthuo999 <thuokelvinngigi@gmail.com>
2024-04-10 12:51:39 +0300

    create: func to handle various promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,16 +2,14 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const promises = [
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName)
-  ];
+  const signUpUserPromise = signUpUser(firstName, lastName);
+  const uploadPhotoPromise = uploadPhoto(fileName);
 
-  return Promise.allSettled(promises)
+  return Promise.allSettled([signUpUserPromise, uploadPhotoPromise])
     .then((results) => {
       return results.map((result) => ({
         status: result.status,
-        value: result.status === 'fulfilled' ? result.value : result.reason.message
+        value: result.status === 'fulfilled' ? result.value : `${result.reason}`
       }));
     });
 }
