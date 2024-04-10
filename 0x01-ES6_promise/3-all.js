0c591f0a86e91e6ccfe7dd069fd484b1e12beb61
kelvinthuo999 <thuokelvinngigi@gmail.com>
2024-04-10 10:32:15 +0300

    create: function to perform signup

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,11 @@
-import { uploadPhoto, createUser } from "./utils.js";
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-    Promise.all([uploadPhoto(), createUser()])
-    .then(([photoResponse, userResponse]) => {
-        console.log('${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}');
-    })
-    .catch(() => {
-        console.log("Signup system offline");
-    });
+  const photo = uploadPhoto();
+  const user = createUser();
+
+  return Promise.all([photo, user]).then((res) => {
+    console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
+  })
+    .catch(() => { console.log('Signup system offline'); });
 }
