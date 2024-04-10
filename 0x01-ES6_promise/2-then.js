16ec4ef341ed72836ac241091278588d2136465b
kelvinthuo999 <thuokelvinngigi@gmail.com>
2024-04-10 10:12:13 +0300

    create: function that utilizes the finally function

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,12 +1,15 @@
 export default function handleResponseFromAPI(promise) {
   return promise
-    .then(() => {
-    console.log("Got a response from the API");
+  .then(() => {
     return {
       status: 200,
-      body: "success"
-    })
+      body: "success",
+    };
+  })
   .catch(() => {
-    console.log("Got a response from the API");
     return new Error();
+  })
+  .finally(() => {
+    console.log("Got a response from the API");
   });
+}
