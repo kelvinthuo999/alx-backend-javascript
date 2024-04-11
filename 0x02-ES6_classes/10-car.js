c3a74c536c9c7f3169c0a176fc79088fba0e9050
kelvinthuo999 <thuokelvinngigi@gmail.com>
2024-04-11 18:57:31 +0300

    create: class car while utilizing symbols

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,28 +1,11 @@
-const CAR_PROPERTIES = Symbol('car_properties');
-
 export default class Car {
   constructor(brand, motor, color) {
-    this[CAR_PROPERTIES] = {
-      brand,
-      motor,
-      color,
-    };
-  }
-
-  get brand() {
-    return this[CAR_PROPERTIES].brand;
-  }
-
-  get motor() {
-    return this[CAR_PROPERTIES].motor;
-  }
-
-  get color() {
-    return this[CAR_PROPERTIES].color;
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
   }
 
   cloneCar() {
-    const { brand, motor, color } = this[CAR_PROPERTIES];
-    return new Car(brand, motor, color);
+    return new Car(this._brand, this._motor, this._color);
   }
 }
