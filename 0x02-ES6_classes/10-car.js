2381748aafe979dfe430c8fc5589564e83f006bd
kelvinthuo999 <thuokelvinngigi@gmail.com>
2024-04-11 18:58:36 +0300

    create: class car while utilizing symbols

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,11 +1,24 @@
+/**
+ * Represents a car with brand, motor, and color attributes.
+ */
 export default class Car {
+  /**
+     * Create a car.
+     * @param {string} brand - The brand of the car.
+     * @param {string} motor - The motor type of the car.
+     * @param {string} color - The color of the car.
+     */
   constructor(brand, motor, color) {
     this._brand = brand;
     this._motor = motor;
     this._color = color;
   }
 
+  /**
+     * Clone the current car.
+     * @returns {Car} A new car instance with the same brand, motor, and color.
+     */
   cloneCar() {
-    return new Car(this._brand, this._motor, this._color);
+    return new this.constructor(this._brand, this._motor, this._color);
   }
 }
