aa02d7fac66cc78f626e12a894c441c0a12a7f4d
kelvinthuo999 <thuokelvinngigi@gmail.com>
2024-04-16 12:25:23 +0300

    create: function to filter set and splice it

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,17 @@
 export default function cleanSet(set, startString) {
-  const filteredArray = Array.from(set).filter((item) => item.startsWith(startString));
-  return filteredArray.join('-').replace(new RegExp(`^${startString}`), '');
+  // Input validation
+  if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
+    return '';
+  }
+
+  const parts = [];
+  for (const value of set.values()) {
+    if (typeof value === 'string' && value.startsWith(startString)) {
+      const valueSubStr = value.substring(startString.length);
+      if (valueSubStr !== value) {
+        parts.push(valueSubStr);
+      }
+    }
+  }
+  return parts.join('-');
 }
