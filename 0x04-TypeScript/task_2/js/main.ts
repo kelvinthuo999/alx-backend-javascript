3bbcc1addd186ac28c7495da7186e1add7eb3759
kelvinthuo999 <thuokelvinngigi@gmail.com>
2024-04-17 19:55:24 +0300

    create: function to check what subject a teacher takes

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -56,4 +56,16 @@ function executeWork(employee: Director | Teacher): string {
     } else {
       return employee.workTeacherTasks();
     }
-}  
+}
+
+type Subjects = 'Math' | 'History';
+
+function teachClass(todayClass: Subjects): string {
+    if (todayClass === 'Math') {
+      return 'Teaching Math';
+    } else if (todayClass === 'History') {
+      return 'Teaching History';
+    } else {
+      throw new Error('Invalid subject');
+    }
+}
