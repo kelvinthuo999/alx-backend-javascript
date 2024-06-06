c3848333b83cd4a496e7dc4291dc464d12b1195c
kelvinthuo999 <thuokelvinngigi@gmail.com>
2024-06-06 06:31:36 +0000

    create: calc function and tests

---
+++ 0x06-unittests_in_js/0-calcul.js
@@ -0,0 +1,8 @@
+// 0-calcul.js
+const calculateNumber = (a, b) => {
+  const roundedA = Math.round(a);
+  const roundedB = Math.round(b);
+  return roundedA + roundedB;
+};
+
+module.exports = calculateNumber;
---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,12 @@
+// 0-calcul.test.js
+const assert = require('assert');
+const calculateNumber = require('./0-calcul.js');
+
+describe('calculateNumber', () => {
+  it('should round numbers and return their sum', () => {
+    assert.strictEqual(calculateNumber(1, 3), 4);
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  });
+});
